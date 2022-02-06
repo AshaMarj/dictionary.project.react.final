d34b041b3c248937c20e84a03fdd81a7951074bc
AshaMarj <crockera2020@gmail.com>
2022-02-05 23:18:09 -0500

    Added opensource link
    Added opensource link and updated App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"> Hello</header>
+        <header className="App-header"> What's the Word of the Day? </header>
         <main>
           <Dictionary />
         </main>
 
-        <footer className="text-center">Coded by Asha Marjani</footer>
+        <footer className="text-center">
+          Coded by Asha Marjani, {""}
+          <a href="https://github.com/AshaMarj/dictionary.project.react.final">
+            open-source code
+          </a>
+        </footer>
       </div>
     </div>
   );
